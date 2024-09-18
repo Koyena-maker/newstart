bcb48959f9642f2d8c04033b44413c3a43eafb5b
Koyena <koyena.kolkata1998@gmail.com>
2024-09-18 20:24:04 +0530

    Final commit from Y

---
+++ src/test/java/Automation/seleniumframeworkpart1/demo.java
@@ -0,0 +1,12 @@
+package Automation.seleniumframeworkpart1;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void lastCommit()
+	{
+		System.out.print("Signing off!!");
+	}
+
+}
